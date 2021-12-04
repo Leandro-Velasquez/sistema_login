a788364f873cef012af07e9065dc7be357a00e1a
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2021-12-04 17:32:02 -0300

    Definimos la estructura html del formulario, creamos la carpeta estilos que contiene nuestra hoja de estilos css

--- index.php
+++ index.php
@@ -5,8 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema login</title>
+    <link rel="stylesheet" href="estilos/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css">
 </head>
 <body>
-    
+    <form class="formulario" method="POST">
+        <i class="far fa-user-circle"></i>
+        <h2 class="formulario__h2">Sign In</h2>
+        <input class="formulario__input" type="text" placeholder="Username">
+        <input class="formulario__input" type="text" placeholder="Password">
+        <button class="formulario__button">Login</button>
+        <div class="formulario__div">
+            <div>
+                <input id="rememberMe" type="checkbox">
+                <label for="rememberMe">Remember me</label>
+            </div>
+            <div>
+                <a href="">Forgot password?</a>
+            </div>
+        </div>
+        <p class="formulario__p">Not a member? <a class="formulario__a" href="">Create Account</a></p>
+    </form>
 </body>
 </html>
