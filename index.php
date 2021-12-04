a9b690c4e4f50093ba8607e87de758377e6da9bf
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2021-12-04 20:41:50 -0300

    Terminamos de darle estilo al index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema login</title>
-    <link rel="stylesheet" href="estilos/styles.css">
+    <link rel="stylesheet" href="estilos/styles.css?1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css">
 </head>
 <body>
