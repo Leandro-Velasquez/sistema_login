add16197c72674b1f586a865279894081a9f65ad
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2021-12-05 14:47:44 -0300

    Definimos el metodo login de la clase Usuario, convertimos a la clase Usuario en una subclase de la clase Db

--- Clases/usuario.php
+++ Clases/usuario.php
@@ -1,6 +1,19 @@
 <?php
-    class Usuario {
-        
+    class Usuario extends Db{
+        public function login($username, $password){
+            if($this->getDatos($username, $password)){
+                header("Location: login.php");
+            }
+            else{
+                ?>
+                <h3 class="formulario__h3-invalid"><i class="fas fa-exclamation-triangle"></i> Invalid username or password</h3>
+                <?php
+            }
+        }
+
+        public function logOut(){
+
+        }
     }
 
 
