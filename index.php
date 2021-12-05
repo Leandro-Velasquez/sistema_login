bb4627e0e14b6e64a91f7360a1fec5f8c56fbd8b
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2021-12-05 15:01:19 -0300

    Agregamos codigo php al index.php debajo del form, le dimos estilos a la etiqueta h3 que notifica que no se pudo iniciar sesion

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     require "Clases/db.php";
-    
+    require "Clases/usuario.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,15 +9,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema login</title>
-    <link rel="stylesheet" href="estilos/styles.css?1">
+    <link rel="stylesheet" href="estilos/styles.css?2">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css">
 </head>
 <body>
     <form class="formulario" method="POST">
+        <?php
+            if(isset($_POST["userName"])){
+                $user = $_POST["userName"];
+                $password = $_POST["password"];
+                $usuario = new Usuario();
+                $usuario->login($user, $password);
+            }
+        ?>
         <i class="far fa-user-circle"></i>
         <h2 class="formulario__h2">Sign In</h2>
-        <input class="formulario__input" type="text" placeholder="Username">
-        <input class="formulario__input" type="text" placeholder="Password">
+        <input class="formulario__input" name="userName" type="text" placeholder="Username">
+        <input class="formulario__input" name="password" type="password" placeholder="Password">
         <button class="formulario__button">Login</button>
         <div class="formulario__div">
             <div>
