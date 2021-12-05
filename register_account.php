6ef8db8f8f13ec63fdbe181d690f3483e2fa097c
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2021-12-04 21:20:15 -0300

    Terminamos de darle estilos al archivo register_account.php

--- register_account.php
+++ register_account.php
@@ -4,9 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Register account</title>
+    <link rel="stylesheet" href="estilos/styles.css?1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css">
 </head>
 <body>
-    
+    <form class="formulario" method="POST">
+        <i class="far fa-user-circle"></i>
+        <h2 class="formulario__h2 formulario__h2--registerAccount">Register Account</h2>
+        <input class="formulario__input" type="text" placeholder="First Name">
+        <input class="formulario__input" type="text" placeholder="Last Name">
+        <input class="formulario__input" type="text" placeholder="Username">
+        <input class="formulario__input" type="text" placeholder="Password">
+        <input class="formulario__input" type="text" placeholder="Confirm Password">
+        <input class="formulario__input" type="text" placeholder="Email Address">
+        <button class="formulario__button">Register</button>
+        <p class="formulario__p--registerAccount">or <a class="formulario__a--registerAccount" href="">Login</a></p>
+    </form>
 </body>
 </html>
