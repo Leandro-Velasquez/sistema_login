844f55d166666df609d2279831af968f0c10451d
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2021-12-11 19:15:39 -0300

    Se creo el archivo recoverPassword.php, se declaro la clase RecoverPassword

---
+++ Clases/recoverPassword.php
@@ -0,0 +1,16 @@
+<?php
+use phpmailer\phpmailer\PHPMailer;
+use phpmailer\phpmailer\Exception;
+
+require dirname(__DIR__)."../phpmailer/phpmailer/src/Exception.php";
+require dirname(__DIR__)."../phpmailer/phpmailer/src/PHPMailer.php";
+require dirname(__DIR__)."../phpmailer/phpmailer/src/SMTP.php";
+require dirname(__DIR__)."../Clases/db.php";
+
+
+
+Class RecoverPassword extends Db{
+
+    
+}
+?>
