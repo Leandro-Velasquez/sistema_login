3425ae80e937b07308c78333b78ac57f66c78c7a
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2021-12-11 19:10:55 -0300

    Se definieron los metodos getId, getUserName, getNewPasswordWithEmailAndToken, setPassword, insertIntoRecoveryTable y deleteRow de la clase Db

--- Clases/db.php
+++ Clases/db.php
@@ -57,6 +57,31 @@
             return $date;
         }
 
+        //Methods GET
+        public function getId($email) {
+            $sql = $this->pdo->prepare("SELECT id FROM usuarios WHERE email=:e");
+
+            $sql->bindParam(":e", $email);
+
+            $sql->execute();
+
+            $date = $sql->fetch(PDO::FETCH_ASSOC);
+
+            return $date?$date['id']:null;
+        }
+
+        public function getUserName($email) {
+            $sql = $this->pdo->prepare("SELECT userName FROM usuarios WHERE email=:e");
+
+            $sql->bindParam(":e", $email);
+
+            $sql->execute();
+
+            $date = $sql->fetch(PDO::FETCH_ASSOC);
+
+            return $date?$date['userName']:null;
+        }
+
         public function getPassword($email){
             $sql = $this->pdo->prepare("SELECT password FROM usuarios WHERE email=:e");
 
@@ -86,6 +111,31 @@
             }
         }
 
+        public function getNewPasswordWithEmailAndToken($email, $token) {
+            $sql = $this->pdo->prepare("SELECT new_password FROM recuperar WHERE email=:e AND token=:t");
+
+            $sql->bindParam(":e", $email);
+            $sql->bindParam(":t", $token);
+
+            $sql->execute();
+
+            $date = $sql->fetch(PDO::FETCH_ASSOC);
+
+            return $date;
+        }
+
+        //Methods SET
+        public function setPassword($email, $newPassword){
+            $pswMd5 = md5($newPassword);
+
+            $sql = $this->pdo->prepare("UPDATE usuarios SET password=:p WHERE email=:e");
+
+            $sql->bindParam(":p", $pswMd5);
+            $sql->bindParam(":e", $email);
+
+            $sql->execute();
+        }
+
         public function insert($firstName, $lastName, $userName, $password, $email){
             $sql = $this->pdo->prepare("INSERT INTO usuarios (firstName, lastName, userName, password, email) VALUES (:f, :l, :u, :p, :e)");
 
@@ -93,6 +143,24 @@
             $sql->bindParam(":l", $lastName);
             $sql->bindParam(":u", $userName);
             $sql->bindParam(":p", $password);
+            $sql->bindParam(":e", $email);
+
+            $sql->execute();
+        }
+
+        public function insertIntoRecoveryTable($email, $newPassword, $token) {
+            $sql = $this->pdo->prepare("INSERT INTO recuperar (email, new_password, token) VALUES (:e, :np, :t)");
+
+            $sql->bindParam(":e", $email);
+            $sql->bindParam(":np", $newPassword);
+            $sql->bindParam(":t", $token);
+
+            $sql->execute();
+        }
+
+        public function deleteRow($email){
+            $sql = $this->pdo->prepare("DELETE FROM recuperar WHERE email=:e");
+
             $sql->bindParam(":e", $email);
 
             $sql->execute();
