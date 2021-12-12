ce9dec657b80f666c207cc5596f0c242c7e74c7d
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2021-12-12 13:13:51 -0300

    Definimos las condiciones para recuperar la contraseña

--- forgot_password.php
+++ forgot_password.php
@@ -1,3 +1,7 @@
+<?php
+    require "Clases/db.php";
+    require "Clases/recoverPassword.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,10 +14,34 @@
 </head>
 <body>
     <form class="formulario" method="POST">
+        <?php
+            if(isset($_POST['email']) && isset($_POST['repeat_email'])){
+                $email = $_POST['email'];
+                $repeatEMail = $_POST['repeat_email'];
+                $db = new Db();
+
+                if($email == $repeatEMail){
+                    if($db->searchEmail($email)){
+                        $recuperar_psw = new RecoverPassword();
+                        $recuperar_psw->sendEmail($email);
+                    }
+                    else{
+                        ?>
+                        <p class="formulario__p-invalid--registerAccount"><i class="fas fa-exclamation-triangle"></i> The email address is not registered</p>
+                        <?php
+                    }
+                }
+                else{
+                    ?>
+                    <p class="formulario__p-invalid--registerAccount"><i class="fas fa-exclamation-triangle"></i> The email addresses do not match</p>
+                    <?php
+                }
+            }
+        ?>
         <i class="far fa-user-circle"></i>
         <h2 class="formulario__h2 formulario__h2--forgot-password">Forgot Password</h2>
-        <input class="formulario__input" type="text" placeholder="Email Address">
-        <input class="formulario__input" type="text" placeholder="Repeat Email">
+        <input class="formulario__input" name="email" type="text" placeholder="Email Address">
+        <input class="formulario__input" name="repeat_email" type="text" placeholder="Repeat Email">
         <button class="formulario__button">Reset Password</button>
         <p class="formulario__p">or <a class="formulario__a" href="index.php">Login</a></p>
     </form>
